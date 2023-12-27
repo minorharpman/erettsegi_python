4c148ff1ab43655cb243de823e28d3622299e314
Zsolt Kunsagi <zsoltkunsagi@gmail.com>
2023-12-27 21:30:50 +0100

    aa

--- 2023_10/lud.py
+++ 2023_10/lud.py
@@ -23,6 +23,19 @@ print("A dobások száma: " + str(len(DobasTomb)))
 
 print("3. feladat ")
 
+sorszam  =1
+legnSorhossz = 0
+legnSorhosszSzama = 0
+for sor  in Sorok:
+    sorhossz = len(sor)
+   # print( sorszam, sorhossz)
+    if sorhossz > legnSorhossz:
+        legnSorhossz = sorhossz
+        legnSorhosszSzama = sorszam
+    sorszam+= 1
+
+print( "Az egyik leghosszabb a(z) " + str(legnSorhosszSzama)+". ösvény, hossza:" + str(legnSorhossz) )
+
 
 
 
