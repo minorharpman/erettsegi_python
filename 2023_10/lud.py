ee167521611ce5d93d22dbdd2512008921cbf081
Zsolt Kunsagi <zsoltkunsagi@gmail.com>
2023-12-31 22:24:57 +0100

    a

--- 2023_10/lud.py
+++ 2023_10/lud.py
@@ -40,7 +40,7 @@ print("4. feladat ")
 osvenySorszam = int(input("Adja meg egy ösvény sorszámát!: "))
 jatekosSzam = int(input("Adja meg a játékosok számát! : "))
 
-#print(osvenySorszam, jatekosSzam)
+# print(osvenySorszam, jatekosSzam)
 
 print("5. feladat ")
 
@@ -54,14 +54,52 @@ adottSorList = list(adottSor)
 szamM = 0
 szamV = 0
 szamE = 0
+sorszam = 1
+
+f = open("kulonleges.txt", "w", encoding="utf-8")
 
 for elem in adottSorList:
-    #print(elem)
+    # print(elem)
     if (elem == "M"): szamM += 1
-    if (elem == "V"): szamV += 1
-    if (elem == "E"): szamE += 1
+    if (elem == "V"):
+        szamV += 1
+        f.write("sorszám: " + str(sorszam) + " \t  típus: V, \n")
+
+    if (elem == "E"):
+        szamE += 1
+        f.write("sorszám: " + str(sorszam) + " \t  típus: E, \n")
+
+    sorszam += 1
+
+print("M: " + str(szamM) + " darab ")
+print("V: " + str(szamV) + " darab ")
+print("E: " + str(szamE) + " darab ")
+
+print("7. feladat ")
+
+print( "játékosok száma:" +str(jatekosSzam))
+
+lepesTarolo = []
+
+for jatekos in range(jatekosSzam) :
+    lepesTarolo.insert(jatekos, [])
 
 
-print("M: "+ str(szamM) + " darab ")
-print("V: "+ str(szamV) + " darab ")
-print("E: "+ str(szamE) + " darab ")
+for i in range(0, len(DobasTomb), jatekosSzam):
+    #print (dobas)
+    for jatekos in range(jatekosSzam) :
+        lepteto = int(jatekos)+int(i)
+        #print(lepteto)
+        #védelem  IndexError: list index out of range
+        try:
+          lepesTarolo[jatekos].append(DobasTomb[lepteto])
+        except:
+          print( "out")
+
+
+
+print(lepesTarolo[0])
+print(lepesTarolo[1])
+print(lepesTarolo[2])
+print(lepesTarolo[3])
+print(lepesTarolo[4])
